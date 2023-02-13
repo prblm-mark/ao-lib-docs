5c20f741247d8b752bd49cfce95708946b12ac64
mark air <mark@prblm.co>
2023-02-13 10:49:26 +0000

    Add Breakpoint Example

--- src/views/BreakpointsView.vue
+++ src/views/BreakpointsView.vue
@@ -1,6 +1,8 @@
 <script setup>
 import BreakpointsTable from '../components/tables/BreakpointsTable.vue'
 import demoBreakpoints from '../data/demoBreakpoints.json'
+import SnippetHTML from '../components/code/SnippetHTML.vue'
+import SnippetSASS from '../components/code/SnippetSASS.vue'
 </script>
 
 <template>
@@ -18,6 +20,32 @@ import demoBreakpoints from '../data/demoBreakpoints.json'
         </p>
 
         <BreakpointsTable :tableData="demoBreakpoints" />
+
+        <p class="mb-40 mt-24 text-md leading-relaxed">
+            Screen size utilities can be applied via a class via appending the
+            required screen size, or via sass with nested mixins, as per the
+            example below.
+        </p>
+
+        <div class="flex-row mb-40">
+            <div class="col-12 col-6@lg pr-10@lg">
+                <SnippetHTML class="h-full">
+                    <template #tag>p</template>
+                    <template #class>fs-sm fs-md@lg</template>
+                    Some text here...
+                </SnippetHTML>
+            </div>
+            <div
+                class="col-12 col-6@lg flex flex-wrap mt-20 mt-0@lg pl-10@lg row-gap-20"
+            >
+                <SnippetSASS>
+                    <template #syntax>@include</template>
+                    <template #breakpoint>lg</template>
+                    <template #nested>fs('md')</template>
+                    fs('sm')
+                </SnippetSASS>
+            </div>
+        </div>
     </div>
 </template>
 
